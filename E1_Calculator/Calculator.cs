69b6970ad46d5714704c5c2e65b291bcd70516ad
Michael <michael.moeller84@gmail.com>
2019-01-30 10:59:05 +0100

    Add project files.

---
+++ E1_Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using static System.Math;
+
+namespace E1_Calculator
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Power(double x, double exp)
+        {
+            return Pow(x, exp);
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
